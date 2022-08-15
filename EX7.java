a8a8a8c0a38312cb28801d61e790dfcc9687b1df
rodrigogal <100000483+rodrigogal@users.noreply.github.com>
2022-08-15 17:35:25 -0300

    Add files via upload

---
+++ EX7.java
@@ -0,0 +1,9 @@
+package com.mycompany.listasigor;
+
+import java.util.Scanner;
+
+public class EX7 {
+            public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        }
+}
